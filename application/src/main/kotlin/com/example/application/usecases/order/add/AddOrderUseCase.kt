37009eb32fdd406869e7dd183a31dccf50382ec2
Ardit Mete <arditmete22@gmail.com>
2023-05-26 21:03:23 +0200

    fix add use case

--- application/src/main/kotlin/com/example/application/usecases/order/add/AddOrderUseCase.kt
+++ application/src/main/kotlin/com/example/application/usecases/order/add/AddOrderUseCase.kt
@@ -9,28 +9,14 @@ import org.springframework.stereotype.Component
 @Component
 class AddOrderUseCase(
     var orderService: IOrderService,
-    var orderMapper: IEntityMapper<AddOrderOutput, Order, >
-): UseCaseExecute<AddOrderInput, AddOrderOutput>{
-        override fun execute(input: AddOrderInput): AddOrderOutput {
-                val order = (Order().apply {
-                    this.name = input.name
-                    this.color = input.color
-                    this.currency = input.currency
-                    this.customerName = input.customerName
-                    this.productName = input.productName
-                    this.quantity = input.quantity
-                })
-            orderService.save(order)
-            val orderEntity = orderMapper.toEntity(order, AddOrderOutput::class.java)
-              return AddOrderOutput().apply {
-                  this.name = orderEntity.name
-                  this.color = orderEntity.color
-                  this.currency = orderEntity.currency
-                  this.customerName = orderEntity.customerName
-                  this.productName = orderEntity.productName
-                  this.quantity = orderEntity.quantity
-              }
-        }
+    var orderMapperInput: IEntityMapper<Order, AddOrderInput>,
+    var orderMapperOutput: IEntityMapper<Order, AddOrderOutput>
+) : UseCaseExecute<AddOrderInput, AddOrderOutput> {
+    override fun execute(input: AddOrderInput): AddOrderOutput {
+        val order = orderMapperInput.toEntity(input, Order::class.java)
+        orderService.save(order)
+        return orderMapperOutput.toObject(order, AddOrderOutput::class.java)
+    }
 }
 
 
